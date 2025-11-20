8f3cfcfbb35f11503dbb1879d58e173bc1dd5b5b
Suraj Kodakandla <kodaksur23@gmail.com>
2025-11-21 00:39:21 +0530

    Add package init file

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+"""
+VisionRAG - CCTV Video Q&A System using RAG
+Version 1.0.0
+
+A system for analyzing CCTV/surveillance videos using:
+- OpenCV for frame extraction
+- BLIP for image captioning  
+- ChromaDB for vector storage
+- Ollama for embeddings and Q&A
+
+Author: AI Generated
+Date: November 2025
+"""
+
+__version__ = "1.0.0"
+__author__ = "VisionRAG Team"
